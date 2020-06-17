670264df4113a270e6eb89de9da17d00f5dc7940
digidigital <bjoern@seipelspace.de>
2020-06-17 20:34:56 +0200

    Create downloader.sh

---
+++ scripts/downloader.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for list in ./downloadlists/*-downloads.txt
+do
+  fileFolder=$(basename --suffix="-downloads.txt" $list)
+  wget -P "./$fileFolder/" --progress=bar -i ./downloadlists/$list
+  unzip "./$fileFolder/*.[zZ][iI][pP]" "*.[pP][kK]3" -d "./$fileFolder" 
+  unzip "./$fileFolder/*.[zZ][iI][pP]" "*.[pP][kK]3" -d "./$fileFolder"
+  rm -f "./$fileFolder/*.[zZ][iI][pP]" 
+done
