0d2f16a1de122cd0844a7bc4ee858ac32e40197f
digidigital <bjoern@seipelspace.de>
2020-06-18 00:15:18 +0200

    Update downloader.sh

--- scripts/downloader.sh
+++ scripts/downloader.sh
@@ -4,6 +4,5 @@ do
   fileFolder=$(basename --suffix="-downloads.txt" $list)
   wget -P "./$fileFolder/" --progress=bar -i $list
   unzip "./$fileFolder/*.[zZ][iI][pP]" "*.[pP][kK]3" -d "./$fileFolder" 
-  unzip "./$fileFolder/*.[zZ][iI][pP]" "*.[pP][kK]3" -d "./$fileFolder"
-  rm -f "./$fileFolder/*.[zZ][iI][pP]" 
+  rm -f ./$fileFolder/*.[zZ][iI][pP] 
 done
