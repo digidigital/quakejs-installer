ee525ed8a78f8b269d02f54c40dc9a144eeb18ec
digidigital <bjoern@seipelspace.de>
2020-06-20 01:54:42 +0200

    Update and rename mapManifestor.sh to manifestor.sh

---
+++ scripts/manifestor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+  
+  file=$1
+
+  # Calculating CRC32 checksum... 
+  checksum=$((0x$(crc32 $file)))
+  
+  # Calculating filesize... 
+  filesize="$(stat -c%s $file)"
+  
+  #Name of the map-file
+  filename="$(basename $file)"
+  basefile="${filename##*-}"
+  folders="$(dirname $filename)"
+  modfolder="${folders##*/}"
+  
+  echo "  {"
+  echo "     \"name\": \"${modfolder}/${basefile}\","
+  echo "     \"compressed\": $filesize,"
+  echo "     \"checksum\": $checksum"
+  echo "  },"
+  
+
