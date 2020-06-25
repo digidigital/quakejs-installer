6685359230c4469a9005ed7b7d085c675828f096
digidigital <bjoern@seipelspace.de>
2020-06-26 01:41:13 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -25,7 +25,6 @@ chmod +x ./scripts/*.sh
 
 ###dpkg-lock-check loop from https://askubuntu.com/questions/132059/how-to-make-a-package-manager-wait-if-another-instance-of-apt-is-running
 i=0
-tput sc
 while fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
     case $(($i % 4)) in
         0 ) j="-" ;;
@@ -33,10 +32,10 @@ while fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
         2 ) j="|" ;;
         3 ) j="/" ;;
     esac
-    tput rc
-    echo -en "\r[$j] Waiting for other software managers to finish..." 
-    sleep 0.5
-    ((i=i+1))
+    clear
+    echo -en "[$j] Waiting for other software managers to finish..." 
+    sleep 0.2
+    i=$((i+1))
 done 
 ###
 apt-get install sudo curl git nodejs npm jq apache2 wget apt-utils screen libarchive-zip-perl zipmerge bash -y
