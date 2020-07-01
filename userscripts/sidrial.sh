c59edfb292fa962b3356e51368201eec3ca0e8d5
digidigital <bjoern@seipelspace.de>
2020-07-02 01:32:30 +0200

    Update sidrial.sh

--- userscripts/sidrial.sh
+++ userscripts/sidrial.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+# installs the sidrial mod on your server
+# check if sidrial file is in mods folder
+# display download - links
+#https://www.per-thormann.de/download/sidrial-v1-1/
+#http://bit.ly/2E9dfIZ
+
+
+# unzip pk3's
+
+# create assets folder on content server
+
+# move files and rename
+
+# delete files in temp
+
+# create sidrial folder in server's base folder
+
+# copy server template
+
+# create multiplayer mapcycle and add to cfg
+
+# create launcher
