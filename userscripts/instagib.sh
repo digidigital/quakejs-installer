d51f8570d293b90958fffb04fdebb746f3164347
digidigital <bjoern@seipelspace.de>
2020-07-02 00:45:54 +0200

    Update instagib.sh
    first idea...

--- userscripts/instagib.sh
+++ userscripts/instagib.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+# unzip mod and move file to assets folder of content server
+unzip -d ./temp ./mods/InstaGib129.0.zip InstaGib129.pk3
+
+mkdir /var/www/html/assets/InstaGib129
+
+mv -f ./temp/InstaGib129.pk3 /var/www/html/assets/InstaGib129/pak100.pk3 
+
+# create start-script
+echo "#!/bin/bash" > /home/$1/quakejs/startinstagib.sh
+echo "su - $1 -c \"cd ~/quakejs && node build/ioq3ded.js +set net_port $3 +set net_ip $2 +set fs_game InstaGib129 +set fs_cdn $4 +set dedicated 1 +exec server.cfg & disown\"" >> /home/$1/quakejs/startinstagib.sh
