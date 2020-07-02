aa81c048489acaf5a69852a9fc9525e499082f1e
digidigital <bjoern@seipelspace.de>
2020-07-03 01:03:23 +0200

    Update instagib.sh

--- userscripts/instagib.sh
+++ userscripts/instagib.sh
@@ -7,9 +7,12 @@ unzip -d ./temp ./mods/InstaGib129.zip InstaGib129.pk3
 mkdir /var/www/html/assets/InstaGib129
 
 mv -f ./temp/InstaGib129.pk3 /var/www/html/assets/InstaGib129/pak100.pk3 
+chmod +r /var/www/html/assets/InstaGib129/pak100.pk3 
 
 # create start-script
 echo "#!/bin/bash" > /home/$1/quakejs/startinstagib.sh
 echo "su - $1 -c \"cd ~/quakejs && node build/ioq3ded.js +set net_port $3 +set net_ip $2 +set fs_game InstaGib129 +set fs_cdn $4 +set dedicated 1 +exec server.cfg & disown\"" >> /home/$1/quakejs/startinstagib.sh
+
+chmod +x /home/$1/quakejs/startinstagib.sh
 
 echo "******Exit Instagib Installer******"
