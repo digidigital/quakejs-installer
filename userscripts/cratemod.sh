ae5b94ecd02503688dd951e070945a980c2c8a2e
digidigital <bjoern@seipelspace.de>
2020-07-03 00:59:50 +0200

    Update cratemod.sh

--- userscripts/cratemod.sh
+++ userscripts/cratemod.sh
@@ -8,6 +8,7 @@ unzip -j -d ./temp ./mods/cratedm3_v1.0.zip cratedm3/pak0.pk3
 mkdir /var/www/html/assets/cratedm3
 
 mv -f ./temp/pak0.pk3 /var/www/html/assets/cratedm3/pak100.pk3 
+cp ./mods/cratemodtextures.zip /var/www/html/assets/cratedm3/pak101.pk3
 
 rm ./temp/*
 # create mod folder in game server's base directory  
@@ -24,5 +25,7 @@ echo 'vstr d1' >> /home/$1/quakejs/base/cratedm3/crateserver.cfg
 # create start-script
 echo "#!/bin/bash" > /home/$1/quakejs/startcratemod.sh
 echo "su - $1 -c \"cd ~/quakejs && node build/ioq3ded.js +set net_port $3 +set net_ip $2 +set fs_game cratedm3 +set fs_cdn $4 +set dedicated 1 +exec crateserver.cfg & disown\"" >> /home/$1/quakejs/startcratemod.sh
+
+chmod +x /home/$1/quakejs/startcratemod.sh
 
 echo "******Exit Cratemod Installer******"
