459c6b59b982c9caa75046c443b8e3c6d6cd7b35
digidigital <bjoern@seipelspace.de>
2020-07-06 00:09:33 +0200

    Update manifestor.sh

--- scripts/manifestor.sh
+++ scripts/manifestor.sh
@@ -1,10 +1,13 @@
 #!/bin/bash 
   
   file=$1
+  currentDir=pwd
 
+  cd "$(dirname $file)" # cd'ing into directory is necessary as workaround because of a bug in crc32 (throws an error in case you have a 8 digit checksum in the filename and use full path)  checksum=$((0x$(crc32 $file))) was so nice :( 
   # Calculating CRC32 checksum... 
-  checksum=$((0x$(crc32 $file)))
-  
+  checksum=$((0x$(crc32 $(basename $file))))
+  cd "$currentDir"
+    
   # Calculating filesize... 
   filesize="$(stat -c%s $file)"
   
