4c56df8c7ebb8f1839372fb185cb02f73b9d7aa6
digidigital <bjoern@seipelspace.de>
2020-07-06 01:03:01 +0200

    Update truecombat.sh

--- userscripts/truecombat.sh
+++ userscripts/truecombat.sh
@@ -20,14 +20,12 @@ mv -f ./temp/map_pak0.pk3 /var/www/html/assets/truecombat/pak103.pk3
 mv -f ./temp/map_pak1.pk3 /var/www/html/assets/truecombat/pak104.pk3
 
 chmod +r /var/www/html/assets/truecombat/*
-# delete files in temp
-rm ./temp/*
 
 # create tc folder in server's base folder
 mkdir /home/$1/quakejs/base/truecombat
 
 # copy server cfgs and mapcycle to server base folder
-unzip -j -d /home/$1/quakejs/base/truecombat ./mods/TrueCombat11Full.zip/truecombat/*.cfg
+unzip -j -d /home/$1/quakejs/base/truecombat ./mods/TrueCombat11Full.zip truecombat/*.cfg
 
 #  copy server cfg.cp ./userscripts/tcserver.cfg /home/$1/quakejs/base/truecombat/
 
@@ -35,6 +33,6 @@ unzip -j -d /home/$1/quakejs/base/truecombat ./mods/TrueCombat11Full.zip/truecom
 echo "#!/bin/bash" > /home/$1/quakejs/starttc.sh
 echo "su - $1 -c \"cd ~/quakejs && node build/ioq3ded.js +set net_port $3 +set net_ip $2 +set fs_game truecombat +set fs_cdn $4 +set dedicated 1 +exec tcserver.cfg & disown\"" >> /home/$1/quakejs/starttc.sh
 
-chmod +x home/$1/quakejs/starttc.sh
+chmod +x /home/$1/quakejs/starttc.sh
 echo "TrueCombat server config location: /home/$1/quakejs/base/truecombat/tcserver.cfg"
 echo "******Exit TrueCombat Installer******"
