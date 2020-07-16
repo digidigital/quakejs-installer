2e1b4bd186ef78415baa099bc8b43391c95b6d86
digidigital <bjoern@seipelspace.de>
2020-07-16 18:01:34 +0200

    Delete padshop-downloader.sh
    no need to download padshop map since it was added to the repository

--- userscripts/padshop-downloader.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget -P "./temp/" --progress=bar https://worldofpadman.net/files/q3map-padshop.zip
-  unzip "./temp/q3map-padshop.zip" "padshop.pk3" -d "./customQ3maps"
-  unzip "./temp/q3map-padshop.zip" "padshop_music.pk3" -d "./pak101input"
-  rm -f ./temp/q3map-padshop.zip 
